a9149de547e95746f1c52a302d3cb970210720cf
Théo Florès <florstheo@gmail.com>
2023-10-03 23:50:18 +0200

    Setup

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
-import colors from '../styles/colors.module.scss'
+"use client";
+
+import colors from '../styles/colors.module.scss';
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Page() {
   return <h1 style={{ color: colors.primaryColor}}>Hello, Next.js!</h1>
